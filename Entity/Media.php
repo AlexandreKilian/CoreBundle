eddfe6422aa9cfada55dd17112869d72ebf5f258
Alexandre Kilian <alexandre@pulpmedia.at>
2014-09-23 10:09:29 +0200

    added Images

---
+++ Entity/Media.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Brix\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Media
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Media
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="source", type="string", length=255)
+     */
+    private $source;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="width", type="integer",nullable=true)
+     */
+    private $width;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="height", type="integer",nullable=true)
+     */
+    private $height;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set source
+     *
+     * @param string $source
+     * @return Media
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    /**
+     * Get source
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * Set width
+     *
+     * @param integer $width
+     * @return Media
+     */
+    public function setWidth($width)
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    /**
+     * Get width
+     *
+     * @return integer
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    /**
+     * Set height
+     *
+     * @param integer $height
+     * @return Media
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    /**
+     * Get height
+     *
+     * @return integer
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+    public function getAbsolutePath()
+    {
+        return null === $this->source
+            ? null
+            : $this->getUploadRootDir().'/'.$this->source;
+    }
+
+    /**
+     * @JMS\VirtualProperty()
+     * @JMS\SerializedName("url")
+     * @JMS\Type("string")
+     */
+    public function getWebPath()
+    {
+        return null === $this->source
+            ? null
+            : $this->getUploadDir().'/'.$this->source;
+    }
+
+    public function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return '/uploads/documents';
+    }
+
+    public function __toString(){
+      return strval($this->getId());
+    }
+}
---
+++ Entity/MediaElement.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Brix\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MediaElement
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class MediaElement
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="media", type="integer")
+     */
+    private $media;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alt", type="string", length=255)
+     */
+    private $alt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="classes", type="string", length=255)
+     */
+    private $classes;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set media
+     *
+     * @param integer $media
+     * @return MediaElement
+     */
+    public function setMedia($media)
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    /**
+     * Get media
+     *
+     * @return integer 
+     */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * Set alt
+     *
+     * @param string $alt
+     * @return MediaElement
+     */
+    public function setAlt($alt)
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    /**
+     * Get alt
+     *
+     * @return string 
+     */
+    public function getAlt()
+    {
+        return $this->alt;
+    }
+
+    /**
+     * Set classes
+     *
+     * @param string $classes
+     * @return MediaElement
+     */
+    public function setClasses($classes)
+    {
+        $this->classes = $classes;
+
+        return $this;
+    }
+
+    /**
+     * Get classes
+     *
+     * @return string 
+     */
+    public function getClasses()
+    {
+        return $this->classes;
+    }
+}
