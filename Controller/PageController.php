4f769fd2e3d6185d871316df7b204a4706ef6593
Alexandre Kilian <alexandre@diamonddev.com>
2014-10-29 11:39:22 +0100

    renamed entity

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -33,9 +33,9 @@ class PageController extends Controller
 
         if($page = $repo->findOneBy(array('url'=>$url))){
 
-            if($page->getLanguage()->getId() != $language->getId() && $page = $repo->findOneBy(array("original"=>$page,"language"=>$language))){
+            if($page->getLanguage()->getId() != $language->getId() && $transpage = $repo->findOneBy(array("original"=>$page,"language"=>$language))){
                 return  $this->forward('BrixCoreBundle:Page:loadPage', array(
-                    'url' => $page->getUrl()
+                    'url' => $transpage->getUrl()
                 ));
             }
 
