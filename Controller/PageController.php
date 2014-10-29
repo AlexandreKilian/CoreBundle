d67e5937c19c4bc994cef08675b8a9cd42725579
Alexandre Kilian <alexandre@diamonddev.com>
2014-10-29 11:06:47 +0100

    added forwarding to translation

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -33,6 +33,12 @@ class PageController extends Controller
 
         if($page = $repo->findOneBy(array('url'=>$url))){
 
+            if($page->getLanguage()->getId() != $language->getId() && $page = $repo->findOneBy(array("original"=>$page,"language"=>$language)))){
+                return  $this->forward('BrixCoreBundle:Page:loadPage', array(
+                    'url' => $page->getUrl();
+                ));
+            }
+
             return $this->renderPage($page);
 
 
