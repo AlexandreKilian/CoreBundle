961fbe8a3aec3f74356f107251d458edafc9fb35
Alexandre Kilian <alexandre@diamonddev.com>
2014-11-12 18:48:42 +0100

    translations for page

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -56,14 +56,56 @@ class PageController extends Controller
 
     }
 
+    private function getTranslations($page){
+
+
+        if($page->getOriginal()){
+            $page = $page->getOriginal();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $repository = $em->getRepository("BrixCoreBundle:Page");
+
+        $qb = $em->createQueryBuilder();
+
+        $qb->select('p.id','p.name','p.url','l.name as locale_name','l.locale')
+            ->from("BrixCoreBundle:Page",'p')
+            ->where('p.id = ?1')
+            ->orWhere('p.original = ?1')
+            ->innerJoin('BrixCoreBundle:Language','l','WITH','p.language = l.id')
+            ->setParameter(1,$page->getId());
+
+        $query = $qb->getQuery();
+
+        // die($query->getSql());
+
+        if($results = $query->getResult()){
+
+        $pages = Array();
+
+        foreach($results as $page){
+            $pages[$page['locale']] = $page;
+        }
+        return $pages;
+
+        }
+
+        return null;
+    }
+
     private function renderPage($page){
         $pageType = $page->getType();
 
+        $translations = $this->getTranslations($page);
+
+
+
         if($pageType->getModel() && $entity = $em->getRepository($pageType->getModel())->find($page->getEntity())){
 
-            return $this->render($pageType->getTemplate(),array('page'=>$page,'entity'=>$entity));
+            return $this->render($pageType->getTemplate(),array('page'=>$page,'entity'=>$entity,'translations'=>$translations));
         }
-        return $this->render($pageType->getTemplate(),array('page'=>$page,'admin'=>$this->isAdminMode()));
+        return $this->render($pageType->getTemplate(),array('page'=>$page,'admin'=>$this->isAdminMode(),'translations'=>$translations));
     }
 
     public function loadHomePageAction(Request $request){
